a88284a16015bd70825a28826b52082f97d6e262
kimhyeyun <cqqwer@gmail.com>
2020-09-02 22:51:26 +0900

    타켓 넘버

--- DFSBFS/DFSBFS/Target_number.cpp
+++ DFSBFS/DFSBFS/Target_number.cpp
@@ -5,13 +5,25 @@
 using namespace std;
 
 int answer = 0;
+int sz;
 
 void DFS(vector<int> & numbers, int target,int sum, int idx) {
-    if(idx==)
+    if (idx == sz) {
+        if (sum ==  target)
+            answer++;
+        return;
+    }
+    DFS(numbers, target, sum + numbers[idx], idx + 1);
+    DFS(numbers, target, sum - numbers[idx], idx + 1);
 }
 
 int solution(vector<int> numbers, int target) {
-    
-
+    sz = numbers.size();
+    DFS(numbers, target, 0, 0);
     return answer;
+}
+
+int main() {
+    vector<int> n = { 1,1,1,1,1 };
+    cout << solution(n, 3);
 }
